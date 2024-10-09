732dae7e49b2aa3c4aa4e2e1b89ee8626b3cd7aa
Victor Vianna <vitorvianna27@gmail.com>
2024-10-09 19:50:54 -0300

    :sparkles: (feat): Second Commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+function MyApp({Component, pageProps}: AppProps) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp;
